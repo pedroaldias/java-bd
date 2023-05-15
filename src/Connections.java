2a1dbc42b4dfa4a862238de08074f45546859d8d
pedroaldias <pedroaffonsoldias@gmail.com>
2023-05-15 17:01:54 -0300

    ifal db

--- src/Connections.java
+++ src/Connections.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class Connections {
     static String url = "jdbc:mysql://localhost:3306/db00";
     static String user = "root";
-    static String password = "1234";
+    static String password = "root";
 
     public static Connection getConnection() {
         try {
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-import java.util.List;
-
-import lib.Helpers;
+// import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Funcionario pedro = new Funcionario();
-        pedro.setId(3L);
-        pedro.setName("Pedro Affonso Lopes Dias");
-        pedro.setAge(19);
-        pedro.setCity("Oslo");
         FuncionarioDao dao = new FuncionarioDao();
-        dao.insert(pedro);
+
+        // Funcionario pedro = new Funcionario();
+        // pedro.setId(3L);
+        // pedro.setName("Pedro Affonso Lopes Dias");
+        // pedro.setAge(19);
+        // pedro.setCity("Oslo");
+        // dao.insert(pedro);
 
         Funcionario matteo = new Funcionario();
         matteo.setId(4L);
@@ -18,24 +17,24 @@ public class Main {
         matteo.setAge(22);
         matteo.setCity("Florence");
         dao.insert(matteo);
-        
-        // dao.query();
+
+        dao.query();
         // dao.getLista().forEach(funcionario -> {
-        //     System.out.println(
-        //             "ID: " + funcionario.getId()
-        //             + "\nNAME: " + funcionario.getName()
-        //             + "\nAGE: " + funcionario.getAge()
-        //             + "\nCITY: " + funcionario.getCity()
-        //             + "\n--------------------------");
+        // System.out.println(
+        // "ID: " + funcionario.getId()
+        // + "\nNAME: " + funcionario.getName()
+        // + "\nAGE: " + funcionario.getAge()
+        // + "\nCITY: " + funcionario.getCity()
+        // + "\n--------------------------");
         // });
         // List<Funcionario> funcionarios = dao.getLista();
         // for(Funcionario funcionario: funcionarios){
-        //     System.out.println(
-        //         "ID: " + funcionario.getId()
-        //         + "\nNAME: " + funcionario.getName()
-        //         + "\nAGE: " + funcionario.getAge()
-        //         + "\nCITY: " + funcionario.getCity()
-        //         + "\n--------------------------");
+        // System.out.println(
+        // "ID: " + funcionario.getId()
+        // + "\nNAME: " + funcionario.getName()
+        // + "\nAGE: " + funcionario.getAge()
+        // + "\nCITY: " + funcionario.getCity()
+        // + "\n--------------------------");
         // }
     }
 }
